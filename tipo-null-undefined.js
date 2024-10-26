b917c0dcc82b7205bcd6ee7cf335c6e6dd440938
samuelbernar <samuelbernardodasilva23@gmail.com>
2024-10-25 21:46:02 -0300

    tipo null e undefined

---
+++ tipo-null-undefined.js
@@ -0,0 +1,9 @@
+let nomeEstudante;
+const professora = 'ana';
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+console.log(typeof professora);
+
+let telefoneEstudante = null;
+
+console.log(typeof telefoneEstudante);
